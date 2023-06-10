863733615ee328af07ebd37a3b7d88320d8d52af
Ellistar <117102466+Ellistar@users.noreply.github.com>
2023-06-10 03:55:37 +0300

    Create Address.java

---
+++ src/main/java/Address.java
@@ -0,0 +1,14 @@
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Setter
+@Getter
+@ToString
+public class Address {
+    private String street;
+    private String suite;
+    private String city;
+    private String zipcode;
+    private Geo geo;
+}
