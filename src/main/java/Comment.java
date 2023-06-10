d525d50b0ddfa784c227801ab5e1a557421e6e84
Ellistar <117102466+Ellistar@users.noreply.github.com>
2023-06-10 03:55:45 +0300

    Create Comment.java

---
+++ src/main/java/Comment.java
@@ -0,0 +1,14 @@
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class Comment {
+    private int postId;
+    private int id;
+    private String name;
+    private String email;
+    private String body;
+}
