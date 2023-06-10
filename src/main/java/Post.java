c5358da396e6c958c45ac359868b3b3081c1a3b2
Ellistar <117102466+Ellistar@users.noreply.github.com>
2023-06-10 03:56:01 +0300

    Create Post.java

---
+++ src/main/java/Post.java
@@ -0,0 +1,11 @@
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Post {
+    private int userId;
+    private int id;
+    private String title;
+    private String body;
+}
