dd628e5de7faf1cd3d5bb70290af85eba6068ff1
Ellistar <117102466+Ellistar@users.noreply.github.com>
2023-06-10 03:56:15 +0300

    Create UT.java

---
+++ src/main/java/UT.java
@@ -0,0 +1,13 @@
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class UT {
+    private int userId;
+    private int id;
+    private String title;
+    private boolean completed;
+}
