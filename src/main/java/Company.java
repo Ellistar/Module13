c262476a237fb2b07123780ef251db77f5b8430d
Ellistar <117102466+Ellistar@users.noreply.github.com>
2023-06-10 03:55:49 +0300

    Create Company.java

---
+++ src/main/java/Company.java
@@ -0,0 +1,12 @@
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Setter
+@Getter
+@ToString
+public class Company {
+    private String name;
+    private String catchPhrase;
+    private String bs;
+}
